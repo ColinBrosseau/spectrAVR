057d39d315501c4352d76a571cee3c5813a33018
Colin-N. Brosseau <colin.brosseau@gmail.com>
2012-12-05 15:40:03 -0500

    nothing

--- ArdU1000.c
+++ ArdU1000.c
@@ -13,12 +13,12 @@
 
 #define setPulse(x) switchToDo = x*2 //set number of pulses to send 
 
-int pulses = LOW; //a pulse is when logic level goes to low
+int pulses = HIGH; //a pulse is when logic level goes to high
 int switchToDo = 0; //number of LED state change remaining
 
 int main(void)
 {
-  setPulse(10);
+  setPulse(10); //create 10 pulses on the led and then stops.
   
   DDRB |= (1 << LED); // Set output on LED pin
   //setup led at default level
@@ -29,9 +29,13 @@ int main(void)
     PORTB |= (1 << LED); // led goes high 
   }
   
-  OCR1A = 7812; //0.5 s per pulse
-  TCCR1B |= (1 << WGM12); // Mode 4, CTC on OCR1A
+  OCR1A = 14062; //at 0.9 s LED low
+  OCR1B = 15624; //at 1 s LED high (pulse duration = 0.1 s)
+  TCCR1A |= (1 << WGM12); // Mode 4, CTC on OCR1A
+  TCCR1B |= (1 << WGM12); // Mode 4, CTC on OCR1B
   TIMSK |= (1 << OCIE1A); //Set interrupt on compare match   
+  TIMSK |= (1 << OCIE1B); //Set interrupt on compare match   
+  TCCR1A |= (1 << CS12) | (1 << CS10); // set prescaler to 1024 and start the timer    
   TCCR1B |= (1 << CS12) | (1 << CS10); // set prescaler to 1024 and start the timer    
   
   sei(); // enable interrupts
@@ -44,8 +48,14 @@ int main(void)
 ISR (TIMER1_COMPA_vect)
 {
   if (switchToDo > 0) {
-    switchToDo -= 1;
     PORTB^= _BV(LED); //toggle LED pin 
   }
 }
 
+ISR (TIMER1_COMPB_vect)
+{
+  if (switchToDo > 0) {
+    switchToDo -= 1;
+    PORTB^= _BV(LED); //toggle LED pin 
+  }
+}
