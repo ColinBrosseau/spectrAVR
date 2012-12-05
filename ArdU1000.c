210751616d7c8e340c13cbad21efa9ebe1712da5
Colin-N. Brosseau <colin.brosseau@gmail.com>
2012-12-05 13:25:27 -0500

    Define active pulse (low level). Create 10 pulses and then stops.

--- ArdU1000.c
+++ ArdU1000.c
@@ -1,5 +1,5 @@
 // ATmega8 @ 16 MHz
-// setup timer1 for 1 s clock cycle
+// setup timer1 for 0.5 s clock cycle
 // blink a led (pin 13 on Arduino)
 //
 //based on https://sites.google.com/site/qeewiki/books/avr-guide/timer-on-the-atmega8
@@ -8,25 +8,44 @@
 #include <avr/interrupt.h>
 
 #define LED PB5 // Define led pin on PB5
+#define HIGH 1 //logic level high
+#define LOW 0 //logic level low
+
+#define setPulse(x) switchToDo = x*2 //set number of pulses to send 
+
+int pulses = LOW; //a pulse is when logic level goes to low
+int switchToDo = 0; //number of LED state change remaining
 
 int main(void)
 {
-    DDRB |= (1 << LED); // Set output on LED pin
-
-    OCR1A = 15624;
-    TCCR1B |= (1 << WGM12); // Mode 4, CTC on OCR1A
-    TIMSK |= (1 << OCIE1A); //Set interrupt on compare match   
-    TCCR1B |= (1 << CS12) | (1 << CS10); // set prescaler to 1024 and start the timer    
-
-    sei(); // enable interrupts
-    
-    while (1);
-    {
-    }
+  setPulse(10);
+  
+  DDRB |= (1 << LED); // Set output on LED pin
+  //setup led at default level
+  if (pulses){
+    PORTB &= ~(1 << LED); // led goes low 
+  }
+  else {
+    PORTB |= (1 << LED); // led goes high 
+  }
+  
+  OCR1A = 7812; //0.5 s per pulse
+  TCCR1B |= (1 << WGM12); // Mode 4, CTC on OCR1A
+  TIMSK |= (1 << OCIE1A); //Set interrupt on compare match   
+  TCCR1B |= (1 << CS12) | (1 << CS10); // set prescaler to 1024 and start the timer    
+  
+  sei(); // enable interrupts
+  
+  while (1);
+  {
+  }
 }
 
 ISR (TIMER1_COMPA_vect)
 {
-  PORTB^= _BV(LED); //toggle LED pin
+  if (switchToDo > 0) {
+    switchToDo -= 1;
+    PORTB^= _BV(LED); //toggle LED pin 
+  }
 }
 
--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
-Toggle a pin on the uC at 1 Hz.
+Create impulsions (an impulsion is low logic level) on a pin of the uC. Pin is alternativelly low and high (with a duration of 0.5 s). Uses interrupt (non blocking). Create 10 pulses and then stops.
 
 At present state, works on an Arduino NG (Atmega8 @ 16 MHz). The pin is PB5 (Arduino pin 13 witch has a built-in led).
