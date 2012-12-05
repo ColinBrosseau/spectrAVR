16b5e202dbc7d8feebd5461b1afce9403a9949e3
Colin-N. Brosseau <colin.brosseau@gmail.com>
2012-12-05 12:33:06 -0500

    Projet description

--- ArdU1000.c
+++ ArdU1000.c
@@ -1,5 +1,5 @@
 // ATmega8 @ 16 MHz
-// sets up timer1 for 1 s clock cycle
+// setup timer1 for 1 s clock cycle
 // blink a led (pin 13 on Arduino)
 //
 //based on https://sites.google.com/site/qeewiki/books/avr-guide/timer-on-the-atmega8
@@ -14,19 +14,14 @@ int main(void)
     DDRB |= (1 << LED); // Set output on LED pin
 
     OCR1A = 15624;
-    TCCR1B |= (1 << WGM12);
-    // Mode 4, CTC on OCR1A
-    TIMSK |= (1 << OCIE1A);
-    //Set interrupt on compare match
-    TCCR1B |= (1 << CS12) | (1 << CS10);
-    // set prescaler to 1024 and start the timer
+    TCCR1B |= (1 << WGM12); // Mode 4, CTC on OCR1A
+    TIMSK |= (1 << OCIE1A); //Set interrupt on compare match   
+    TCCR1B |= (1 << CS12) | (1 << CS10); // set prescaler to 1024 and start the timer    
 
-    sei();
-    // enable interrupts
-
+    sei(); // enable interrupts
+    
     while (1);
     {
-        // we have a working Timer
     }
 }
 
---
+++ Readme.md
@@ -0,0 +1,3 @@
+Toggle a pin on the uC at 1 Hz.
+
+At present state, works on an Arduino NG (Atmega8 @ 16 MHz). The pin is PB5 (Arduino pin 13 witch has a built-in led).
