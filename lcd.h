6072cfc634108c62187e410555a25efad1dd1a0d
Colin-N. Brosseau <colin.brosseau@gmail.com>
2012-12-09 03:52:31 +0000

    must define F_CPU for lcd.c

--- lcd.h
+++ lcd.h
@@ -44,8 +44,9 @@
  *  @name  Definitions for MCU Clock Frequency
  *  Adapt the MCU clock frequency in Hz to your target. 
  */
-#define XTAL 16000000              /**< clock frequency in Hz, used to calculate delay timer */
-
+#ifndef F_CPU
+   #define F_CPU 16000000UL              /**< clock frequency in Hz, used to calculate delay timer */
+#endif
 
 /**
  * @name  Definition for LCD controller type
