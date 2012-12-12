00da9d195038294d765b2664d898a424b4560608
Colin-N. Brosseau <colin.brosseau@gmail.com>
2012-12-12 00:24:56 -0500

    correction to git management script

--- gitFetchAll.sh
+++ gitFetchAll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master`; do
-    git checkout ${branch##*/} $branch
-#    git branch --track ${branch##*/} $branch
+#    git checkout ${branch##*/} $branch
+    git branch --track ${branch##*/} $branch
 done
