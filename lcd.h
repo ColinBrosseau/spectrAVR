f4e71983deb43a55ef021c05494d0a93bde8b983
Colin-N. Brosseau <colin.brosseau@gmail.com>
2012-12-14 10:23:56 -0500

    lcd changed from portD to portC

--- lcd.h
+++ lcd.h
@@ -84,19 +84,19 @@
  *  ports by adapting the LCD_DATAx_PORT and LCD_DATAx_PIN definitions.
  *  
  */
-#define LCD_PORT         PORTD        /**< port for the LCD lines   */
+#define LCD_PORT         PORTC        /**< port for the LCD lines   */
 #define LCD_DATA0_PORT   LCD_PORT     /**< port for 4bit data bit 0 */
 #define LCD_DATA1_PORT   LCD_PORT     /**< port for 4bit data bit 1 */
 #define LCD_DATA2_PORT   LCD_PORT     /**< port for 4bit data bit 2 */
 #define LCD_DATA3_PORT   LCD_PORT     /**< port for 4bit data bit 3 */
-#define LCD_DATA0_PIN    4            /**< pin for 4bit data bit 0  */
-#define LCD_DATA1_PIN    5            /**< pin for 4bit data bit 1  */
-#define LCD_DATA2_PIN    6            /**< pin for 4bit data bit 2  */
-#define LCD_DATA3_PIN    7            /**< pin for 4bit data bit 3  */
+#define LCD_DATA0_PIN    3            /**< pin for 4bit data bit 0  */
+#define LCD_DATA1_PIN    2            /**< pin for 4bit data bit 1  */
+#define LCD_DATA2_PIN    1            /**< pin for 4bit data bit 2  */
+#define LCD_DATA3_PIN    0            /**< pin for 4bit data bit 3  */
 #define LCD_RS_PORT      LCD_PORT     /**< port for RS line         */
-#define LCD_RS_PIN       2            /**< pin  for RS line         */
+#define LCD_RS_PIN       5            /**< pin  for RS line         */
 #define LCD_RW_PORT      LCD_PORT     /**< port for RW line         */
-#define LCD_RW_PIN       3            /**< pin  for RW line         */
+#define LCD_RW_PIN       4            /**< pin  for RW line         */
 #define LCD_E_PORT       PORTB     /**< port for Enable line     */
 #define LCD_E_PIN        0            /**< pin  for Enable line     */
 
