36dd72972f28ecf12e8b88f7d8e4540d936dbaef
Colin-N. Brosseau <colin.brosseau@gmail.com>
2013-01-18 12:28:43 -0500

    fix header

--- uartParser.h
+++ uartParser.h
@@ -2,5 +2,5 @@ void copy_command(void);
 void process_command(void);
 void print_value (char *id, int value);
 void uart_ok(void);
-int parse_assignment (char input[16]);
+long parse_assignment (char input[16]);
 void process_uart(void);
