c7235556ee18255d25e7bffeffaac028118f2b25
Colin-N. Brosseau <colin.brosseau@gmail.com>
2013-03-11 17:11:06 -0400

    m

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 
 #define step2position 200 //convert number of steps in physical position (here Angstroms)
 //pulses speed and acceleration
-int N = 50; //number of pulses to fully accelerate. 50 semble correct
+int N = 500; //number of pulses to fully accelerate. 50 semble correct
 int speedLow = 11000; //minimum speed (actually period)
 int speedFast = 7000; //maxmimum speed (actually period). <6500 too low, 7000 correct
 
@@ -103,6 +103,8 @@ int main(void) {
   lcd_clrscr(); /* clear display and home cursor */
   lcd_puts("AVR U1000"); /* put string to display (line 1) with linefeed */ 
         
+  setPulse(5000);
+
   while(1) {
     _delay_ms(200);
     // LCD display
