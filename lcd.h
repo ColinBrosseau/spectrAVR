e51319145d0a05838c44bb7abee4ba6001b55e2e
Colin-N. Brosseau <colin.brosseau@gmail.com>
2013-03-26 11:24:06 -0400

    unified U1000 and HR320 LCD pin configuration

--- lcd.h
+++ lcd.h
@@ -90,23 +90,7 @@
  *  ports by adapting the LCD_DATAx_PORT and LCD_DATAx_PIN definitions.
  *  
  */
-#if defined(U1000)
-  #define LCD_PORT         PORTC        /**< port for the LCD lines   */
-  #define LCD_DATA0_PORT   LCD_PORT     /**< port for 4bit data bit 0 */
-  #define LCD_DATA1_PORT   LCD_PORT     /**< port for 4bit data bit 1 */
-  #define LCD_DATA2_PORT   LCD_PORT     /**< port for 4bit data bit 2 */
-  #define LCD_DATA3_PORT   LCD_PORT     /**< port for 4bit data bit 3 */
-  #define LCD_DATA0_PIN    4            /**< pin for 4bit data bit 0  */
-  #define LCD_DATA1_PIN    5            /**< pin for 4bit data bit 1  */
-  #define LCD_DATA2_PIN    6            /**< pin for 4bit data bit 2  */
-  #define LCD_DATA3_PIN    7            /**< pin for 4bit data bit 3  */
-  #define LCD_RS_PORT      LCD_PORT     /**< port for RS line         */
-  #define LCD_RS_PIN       3            /**< pin  for RS line         */
-  #define LCD_E_PORT       LCD_PORT     /**< port for Enable line     */
-  #define LCD_E_PIN        2            /**< pin  for Enable line     */
-  #define LCD_RW_PORT      PORTD        /**< port for RW line         */
-  #define LCD_RW_PIN       7            /**< pin  for RW line         */
-#elif defined(HR320)
+#if defined(U1000) || defined(HR320)
   #define LCD_PORT         PORTC        /**< port for the LCD lines   */
   #define LCD_DATA0_PORT   LCD_PORT     /**< port for 4bit data bit 0 */
   #define LCD_DATA1_PORT   LCD_PORT     /**< port for 4bit data bit 1 */
